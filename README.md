9e596e86ed3cd7723660e557efe8419d8a1b6896
senthil100695 <senthil100695@gmail.com>
2023-09-12 16:25:59 +0530

    prepare base model completed

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Chicken_disease_classification
+
+
+#Work_flows of this project
+
+1. Update the config.yaml
+2. Update the secrets.yaml [optional]
+3. Update the params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+9. Update the dvc.yaml
+
---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,21 @@
+#step 4 : create the entity -> return type of the function 
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen = True)
+class DataIngestionConfig:
+    root_dir : Path
+    source_URL : str
+    local_data_file : Path
+    unzip_dir : Path
+    
+@dataclass(frozen = True)
+class PrepareBaseModelConfig:
+    root_dir : Path
+    base_model_path : Path
+    updated_base_model_path : Path
+    params_image_size : list
+    params_learning_rate : float
+    params_include_top : bool
+    params_weights : str
+    params_classes : int
